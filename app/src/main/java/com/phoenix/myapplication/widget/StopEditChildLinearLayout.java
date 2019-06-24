68616eab8145f64f14997fe9830dc6146e765e80
caoyl <caoyl@hvlin.com>
2019-06-24 20:42:15 +0800

    添加文件

---
+++ app/src/main/java/com/phoenix/myapplication/widget/StopEditChildLinearLayout.java
@@ -0,0 +1,52 @@
+package com.phoenix.myapplication.widget;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.LinearLayout;
+
+/**
+ * 子控件是否可以接受点击事件
+ */
+public class StopEditChildLinearLayout extends LinearLayout {
+
+    private boolean childClickable = true;
+
+    public StopEditChildLinearLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public StopEditChildLinearLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public StopEditChildLinearLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    public StopEditChildLinearLayout(Context context) {
+        super(context);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        //返回true则拦截子控件所有点击事件，如果childclickable为true，则需返回false
+        return !childClickable;
+    }
+
+    public void setChildClickable(boolean clickable) {
+        childClickable = clickable;
+    }
+
+    /**
+     * 获取子节点是否可点击
+     *
+     * @return
+     */
+    public boolean getChildClickable() {
+        return childClickable;
+    }
+}
